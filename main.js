ee7fa09ad3b7d6bcc4fc4e0a99355970c77e00f6
Tim Foley <thetimfoley@gmail.com>
2016-09-17 01:31:13 -0400

    updated win condition to ignore middle column

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+//QUESTIONS:
+  // should I daisy chain my methods, or have them call in sequence?
+
 $rings = $('.ring');
 $columns = $('.col');
+$winningColumns = $('.c1,.c3')
 
 $columns.on('click', function(){
   game.click($(this));
@@ -16,11 +20,6 @@ $('.col').mouseleave(function(){
 });
 
 var game = {
-  columns: {
-    c1: [],
-    c2: [1,2,3,4],
-    c3: [],
-  },
   rings: 4,
   moves: 0,
   active: false,
@@ -30,7 +29,6 @@ var game = {
   targetId: 0,
   click: function(clicked) {
     clickedRing = clicked.children().eq(0);
-    console.log(clickedRing);
     if (!this.active) {
       game.active = true;
       clickedRing.addClass('active');
@@ -38,50 +36,42 @@ var game = {
       this.moverId = clickedRing.attr('id');
     } else if (this.checkMove(clickedRing)){
       this.moveRing(clicked);
+      this.checkWin();
+    } else {
+      this.softReset();
     }
 
   },
   checkMove: function(target) {
-    // if originCol's firstchild's id > target's firstchild's id, make move
-    // ringToMove = this.originCol.children().eq(0).attr('id');
     this.targetId = target.hasClass('ring') ? target.attr('id') : 100;
     if (this.moverId < this.targetId) {
       return true;
     } else if (this.moverId == this.targetId){
-      this.softReset();
       return false;
     } else {
-      console.log("That's not a legal move");
-      this.softReset();
       return false;
     }
     // else give indication that move isn't legal (animation or something)
   },
   moveRing: function(destination) {
-    // increment moves, move ring in DOM, set active to false
     this.moves++;
     $('.active').prependTo(destination);
     this.softReset();
-    // TODO move to the proper array in game.columns
-    // try using .map?
-
   },
   softReset: function() {
     $rings.removeClass('active hover');
     this.active = false;
   },
   checkWin: function() {
-    for (col in this.columns) {
-      console.log(col, this.columns[col].length);
-      if (this.columns[col].length === this.rings) {
-        this.gameOver();
-        return true;
+    $winningColumns.each(function(column){
+      if ($(this).children().length == game.rings) {
+        game.gameOver();
+        return false;
       }
-      console.log("You haven't won yet");
-    }
+    })
   },
   gameOver: function() {
-    console.log("You WIN!");
+    console.log("You won in " + this.moves + " moves!");
   },
 
 }
