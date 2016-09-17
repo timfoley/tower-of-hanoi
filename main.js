cb3f165cb90d36c76685385bc8e6f849f6a0f3bf
Tim Foley <thetimfoley@gmail.com>
2016-09-16 23:32:09 -0400

    started outlining object and methods

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+$rings = $('.rings')
+
+var towers = {
+  columns: {
+    col1: [],
+    col2: [],
+    col3: [],
+  },
+  rings: 4,
+  moves: 0,
+  moveRing: function(ring, destination) {
+    ring.appendTo(destination)
+  },
+  checkMove: function() {},
+  checkWin: function() {
+    for (col in this.columns) {
+      if (this.length === this.rings) {
+        this.gameOver();
+      } else {
+        return false
+      }
+    }
+  },
+  gameOver: function() {},
+
+}
+
 $('.col').hover(function(){
   console.log('HOVER');
-  $(this).children('.ring').first().toggleClass('active');
+  $(this).children('.ring').first().toggleClass('hover');
 })
+
+// when you click on a column,
+// add active class to top ring and toggle active variable
